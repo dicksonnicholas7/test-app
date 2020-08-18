559dab7ef2d3adba4ef5c3d0ec4ad204352465a2
Nicholas Dickson <58767603+dicksonnicholas7@users.noreply.github.com>
2020-08-18 13:58:57 +0000

    added nodemon module

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  return res.status(200).json({ message: 'run' });
 });
+
 
 module.exports = router;
